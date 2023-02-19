54bccea7f73629d41e235f31e7bd712178a9e0d8
shiyiawei <1078107814@qq.com>
2023-02-19 14:45:00 +0800

    head更新

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,27 +1,31 @@
-export default {
+ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'fangjuejin',
+    title: '仿掘金网站',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: '基于NUXTJS及NodeJS开发' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/juejin.png' }
     ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    //针对elementui引入
+    'element-ui/lib/theme-chalk/index.css',
+    'element-ui/lib/theme-chalk/display.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '~/plugins/element-ui.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -37,5 +41,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    transpile: [/^element-ui/],
   }
 }
